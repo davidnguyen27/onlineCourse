39a2d1057fc47fa8f057a20bfb23f4b9fea1bfc1
davidnguyen27 <theanhngujen2702@gmail.com>
2024-06-05 08:24:19 +0700

    Update sider

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import { Layout } from 'antd';
-import Sider from 'antd/es/layout/Sider';
-import { Content, Footer, Header } from 'antd/es/layout/layout';
-import Navbar from './components/Navbar';
-import AppFooter from './components/AppFooter';
-import AppSider from './components/AppSider';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <Layout className="h-screen w-screen flex flex-col">
-      <Header className="header">
-        <Navbar />
-      </Header>
-      <Layout className="flex flex-1">
-        <Sider className="sider">
-          <AppSider />
-        </Sider>
-        <Layout className="flex flex-col flex-1">
-          <Content className="flex-1">
-            <h1 className="text-red-700">Content</h1>
-          </Content>
-          <Footer className="footer">
-            <AppFooter />
-          </Footer>
-        </Layout>
-      </Layout>
-    </Layout>
+    <>
+      <HomePage />
+    </>
   );
 };
 
--- src/components/AppSider.tsx
+++ src/components/AppSider.tsx
@@ -1,7 +1,32 @@
-const AppSider = () => {
+const AppSider: React.FC<{ className?: string }> = () => {
   return (
     <>
-      <div className=""></div>
+      <div className="">
+        <div className="sider-item active">
+          <i className="fa-solid fa-house"></i>
+          <span className="sider-text">Home</span>
+        </div>
+        <div className="sider-item">
+          <i className="fa-solid fa-address-card"></i>
+          <span className="sider-text">About</span>
+        </div>
+        <div className="sider-item">
+          <i className="fa-solid fa-layer-group"></i>
+          <span className="sider-text">Categories</span>
+        </div>
+        <div className="sider-item">
+          <i className="fa-solid fa-gear"></i>
+          <span className="sider-text">Setting</span>
+        </div>
+        <div className="sider-item">
+          <i className="fa-solid fa-circle-question"></i>
+          <span className="sider-text">Help</span>
+        </div>
+        <div className="sider-item">
+          <i className="fa-solid fa-flag"></i>
+          <span className="sider-text">Report</span>
+        </div>
+      </div>
     </>
   );
 };
